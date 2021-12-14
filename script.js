f1ed0477ade81caa8817f08e3beb7805f4b8ac71
Mohit <mrpundir28@gmail.com>
2021-12-15 01:03:56 +0530

    sdfghsdfg

--- script.js
+++ script.js
@@ -1,52 +1,89 @@
 var bowl, bowlImage;
-var fruit, cherry, orange, banana;
-var fruitList;
+var cherry, orange, banana;
+var packetList;
+
+var greenPacket;
+var yellowPacket;
+var bombPacket;
+
 var backgroundImage;
 var count =0;
 var life=3;
 
+var isDrawing = false;
+var userSelectedPacket = "";
 //preload all the sprites
 function preload() {
-  /*bowlImage = loadImage("bowl.png");
-  cherry = loadImage("cherry.png");
-  orange = loadImage("orange.png");
-  banana = loadImage("banana.png");
-*/
-bowlImage = loadImage("robber.png");
+  bowlImage = loadImage("robber.png");
 
   cherry = loadImage("bomb.png");
   orange = loadImage("coin.png");
   banana = loadImage("coin1.png");
   backgroundImage = loadImage("bg.jpg");
+
+  var params = location.href.split('?')[1].split('&');
+  data = {};
+  for (x in params)
+  {
+  data[params[x].split('=')[0]] = params[x].split('=')[1];
+  }
+  console.log('received data is :' + data.packet);
+  userSelectedPacket = data.packet;
 }
 function setup() {
   createCanvas(screen.width - 20, screen.height - 20 );
   //Create bowl sprite
-  bowl = createSprite(200, screen.height - 50, 20, 20);
+  bowl = createSprite(200, screen.height - 70, 20, 20);
+  bowl.scale = 0.65;
   bowl.addImage(bowlImage);
-  //Array of fruits
-  fruitList = [cherry,orange,banana];
-  //Create random fruit sprites
-  fruit = createSprite(50,20,20,20);
-  fruit.addImage(random(fruitList));
+
+  packetList = [cherry,orange,banana];
+  createRandom();
+
 }
 
 function mouseDragged(event) {
   //console.log(event.touches[0].clientX);
   bowl.position.x=event.touches[0].clientX;
 }
+setInterval(function(){ 
+  //code goes here that will be run every 5 seconds. 
+  if(isDrawing){
+    createRandom();
+  }
+  isDrawing = false;
 
+}, 2000);
 function draw() {
+  isDrawing = true;
   background(backgroundImage);
-  fruit.position.y = fruit.position.y + 3;
-  //Generate random fruit sprite
-  if (fruit.position.y > height) 
+  //Generate random greenPacket sprite
+
+  if(greenPacket!=null){
+    greenPacket.position.y = greenPacket.position.y + 3.5;
+
+    if (greenPacket.position.y > height) 
     {
-      life--;
-      removeSprite( fruit );
-      fruit = createSprite(random(10, width-10),20,20,20);
-      fruit.addImage(random(fruitList));
+      removeSprite( greenPacket );
     }
+  }
+  if(yellowPacket!=null){
+    yellowPacket.position.y = yellowPacket.position.y + 3.5;
+
+    if (yellowPacket.position.y > height) 
+    {
+      removeSprite( yellowPacket );
+    }
+  }
+  if(bombPacket!=null){
+    bombPacket.position.y = bombPacket.position.y + 3.5;
+
+    if(bombPacket.position.y>height)
+    {
+      removeSprite( bombPacket );
+    }
+  }
+  
   //Move the bowl to the right and left
   if (keyDown(RIGHT_ARROW) && bowl.position.x < (width - 50))
     {
@@ -56,28 +93,64 @@ function draw() {
     {
       bowl.position.x -= 20;
     }
+
+
   /* Moving the bowl using mouseX
   bowl.position.x=mouseX; */
   bowl.position.x=mouseX;
-  //Catch fruit in the bowl
-  if (fruit.overlap(bowl)) 
+
+  if(greenPacket!=null){
+
+  //Catch greenPacket in the bowl
+  if (greenPacket.overlap(bowl)) 
     {
       count++; 
-      removeSprite( fruit );
-      fruit = createSprite(random(10, width-10),20,20,20);
-      fruit.addImage(random(fruitList));
+      removeSprite( greenPacket );
+      console.log('greenpacket');
+      if(userSelectedPacket != "green"){
+        life = 0;
+      }
     }
+  }
+
+  if(yellowPacket!=null){
+  //Catch yellowPacket in the bowl
+  if (yellowPacket.overlap(bowl)) 
+    {
+      count++; 
+      removeSprite( yellowPacket );
+      
+      console.log('yellowPacket');
+      if(userSelectedPacket != "yellow"){
+        life = 0;
+      }
+    }
+  }
+
+  if(bombPacket!=null){
+  //Catch bomb in the bowl
+  if (bombPacket.overlap(bowl)) 
+    {
+      count++; 
+      removeSprite( bombPacket );
+      life = 0;
+      console.log('bombPacket');
+
+    }
+  }
   //Display Score and Life
   fill("white");
-  textSize(10);
-  text("SCORE:"+count, 10,40);
-  text("LIFE:"+life,500,40);
+  textSize(15);
+  text("Score:" + count, 20,50);
+  //text("LIFE:"+life,500,40);
   //Game Over condition
   if(life<=0)
     {
       textSize(20);
       text("GAME OVER",50,300);
       noLoop();
+      parent.window.postMessage("score:" + count , "*");
+      parent.window.postMessage("removetheiframe", "*");
     }
     drawSprites();
 }
@@ -87,4 +160,31 @@ function mousePressed()
   count=0;
   life=3;
   loop();
+}
+var lastpacket = 0;
+function createRandom(){
+  var n = (Math.floor(Math.random() * packetList.length));
+
+  if(n==0){
+    if(lastpacket == 0){createRandom(); return;}
+    bombPacket = createSprite(random(10, width-10),30,20,20);
+    bombPacket.scale = 0.65;
+    bombPacket.addImage(cherry);
+    lastpacket = 0;
+  }
+  if(n==1){
+    if(lastpacket == 1){createRandom(); return;}
+    greenPacket = createSprite(random(10, width-10),30,20,20);
+    greenPacket.scale = 0.65;
+    greenPacket.addImage(orange);
+    lastpacket = 1;
+  }
+  if(n==2){
+    if(lastpacket == 2){createRandom(); return;}
+    yellowPacket = createSprite(random(10, width-10),30,20,20);
+    yellowPacket.scale = 0.65;
+    yellowPacket.addImage(banana);
+    lastpacket = 2;
+  }
+
 }
